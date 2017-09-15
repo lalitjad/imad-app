d965d2143833b599ff6d2d1a205145dad9e01c0a
lalitjad98 <30873825+lalitjad98@users.noreply.github.com>
2017-09-16 01:34:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ var articles= {
                 <p>
                     This is content of my first article. This is content of my first article.This is content of my first article.This is content of my first article.This is content of my first article.This is content of my first article.This is content of my first article.This is content of my first article.This is content of my first article.
                 </p>`
-},
+    },
     'article-two': { 
     title: 'Article Two | LALIT JADHAV',
     heading: 'Article Two',
@@ -31,50 +31,52 @@ var articles= {
     'article-three': {
     title: 'Article Three | LALIT JADHAV',
     heading: 'Article Three',
-    date: 'Aug 12,2017',
+    date: 'Aug 15,2017',
     content: `  <p>
                     This is content of my third article.
                 </p>
 `}
 };
 
-function createTemplate(data){
+function createTemplate (data) {
     var title= data.title;
     var heading= data.heading;
     var date= data.date;
     var content= data.content;
-var htmlTemplate=`
-    <html>
-    <head>
-    <title>
-        ${title}
-    </title>
-    <meta name="viewport" content="width-device-width initial-scale-1">
-    <link href="/ui/style.css" rel="stylesheet" />
-    </head>
-    <body>
-        <div class="container">
-            <div>
-                <a herf="/">Home</a>
+    var htmlTemplate=`
+        <html>
+        <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" content="width-device-width initial-scale-1">
+        <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class="container">
+                <div>
+                    <a herf="/">Home</a>
+                </div>
+                <hr/>
+                <h3>
+                   ${heading}
+                </h3>
+                <div>
+                    ${date}
+                </div>
+                <div>
+                    ${content}
+                </div>
             </div>
-            <hr/>
-            <h3>
-               ${heading}
-            </h3>
-            <div>
-                ${date}
-            </div>
-            <div>
-                ${content}
-            </div>
-        </div>
-    </body>
-</html>
-`;
-return htmlTemplate;
+        </body>
+    </html>
+    `;
+    return htmlTemplate;
 }
 
 app.get('/:articleName', function (req, res) {
+    //articleName== article-one
+    //articles[aticleName]=={} content object for article one
     res.send(createTemplate(articles[articleName]));
   var articleName = req.params.articleName;
 });
