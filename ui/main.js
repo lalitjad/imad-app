b07604306d2d6ec85600be55d4fb711334883dde
lalitjad98 <30873825+lalitjad98@users.noreply.github.com>
2017-09-15 19:17:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 
 //Change the text of main-text division
 var element= document.getElementById('main-text');
-element.innerHTML = "New Value";
+element.innerHTML = "Welcome! \n Just tap on Dragon";
 
 
 //move image 
