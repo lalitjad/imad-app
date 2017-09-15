69e7b47122d7e63744f743f4fda8a0837dc7c324
lalitjad98 <30873825+lalitjad98@users.noreply.github.com>
2017-09-15 18:43:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,4 +2,4 @@ console.log('Loaded!');
 
 //Change the text of main-text division
 var element= document.getElementByID('main-text');
-element.innerHTML="New Value";
+element.innerHTML = "New Value";
