2459a9dc3c38bd813706f1e117c68364a996e2c7
lalitjad98 <30873825+lalitjad98@users.noreply.github.com>
2017-09-15 18:59:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,10 @@ console.log('Loaded!');
 //Change the text of main-text division
 var element= document.getElementById('main-text');
 element.innerHTML = "New Value";
+
+
+//move image 
+var img= document.getElementById('madi');
+img.onclick = function(){
+    img.style.marginLeft = '100px';
+};
