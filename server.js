d3aa5e55ddc564c9550ca10fd4e6dc6ad09fb484
lalitjad98 <30873825+lalitjad98@users.noreply.github.com>
2017-09-17 10:28:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -103,7 +103,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-end(/:articleName)
+app.get('/:articleName', function (req, res) {
+    //articleName== article-one
+    //articles[aticleName]=={} content object for article one
+    res.send(createTemplate(articles[articleName]));
+  var articleName = req.params.articleName;
+});
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
