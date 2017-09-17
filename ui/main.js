506db99c2eed0db9fe43cc5e62fa7f9bda8a0dcc
lalitjad98 <30873825+lalitjad98@users.noreply.github.com>
2017-09-17 23:36:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,15 @@
-console.log('Loaded!');
+//Counter code
+var button = getElementById('counter');
+var counter=0;
 
-//Change the text of main-text division
-var element= document.getElementById('main-text');
-element.innerHTML = "Welcome! \n Just tap on Dragon";
-
-
-//move image 
-var img= document.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
-    marginLeft= marginLeft + 1;
-    img.style.marginLeft= marginLeft +'px';
-}
-img.onclick = function(){
-    var interval=setInterval(moveRight,10);
+button.onclick = function() {
+    //Make a request to counter endpoint.
+    
+    //Capture the response and store it in a variable.
+    
+    //Render the variable in a correct span.
+    counter=counter+1;
+    var span=document.getElementById('count');
+    span.innerHTML= counter.toString();
+    
 };
