4f480656047169243b4fe2369d56a8944a326d60
lalitjad98 <30873825+lalitjad98@users.noreply.github.com>
2017-09-30 01:25:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,10 +27,10 @@ submit.onclick=function(){
     
     var names=['name1','name2','name3', 'name4'];
     var list='';
-    for(var i=0;i<names.length;i++){
+    for(var i=0; i<names.length; i++){
     list += '<li>' + names[i] + '<li>';
 }
-var ui= document.getElementById('namelist');
+var ul= document.getElementById('namelist');
 ul.innerHTML=list;
 };
 
