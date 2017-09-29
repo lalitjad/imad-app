84f62d5ded9adc08d57e56e75222a7a537243d66
lalitjad98 <30873825+lalitjad98@users.noreply.github.com>
2017-09-30 00:31:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,21 @@
 //Counter code
 var button = document.getElementById('counter');
 
-var counter=0;
 button.onclick = function() {
 
-    counter=counter+1;
+    var request= new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        
+    if(request.readystate===XMLHttpRequest.DONE){
+        if(request.status===200){
+            var counter = request.responceText;
     var span= document.getElementById('count');
     span.innerHTML = counter.toString();
+            }
+        }
+    };
+    
+    request.open('GET','http://lalitjad98.imad.hasura-app.io/counter',true);
+    request.send(null);
+    
 };
