23e616f01807ff5a5d9b41b688c4854a0068fa98
lalitjad98 <30873825+lalitjad98@users.noreply.github.com>
2017-10-02 18:38:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,7 +32,7 @@ submit.onclick=function(){
         if(request.status===200){
     
     var names=request.responseText;
-    reuest=JSON.porse(names);
+    reuest=JSON.parse(names);
     var list='';
     for(var i=0; i<names.length; i++){
     list += '<li>' + names[i] + '<li>';
