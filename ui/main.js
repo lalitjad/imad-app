bb9bd4c81313fd6fb0ba3b628d861ee13f374dcf
lalitjad98 <30873825+lalitjad98@users.noreply.github.com>
2017-10-02 18:14:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,12 +25,25 @@ var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
     
+        var request= new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        
+    if(request.readyState===XMLHttpRequest.DONE){
+        if(request.status===200){
+    
     var names=['name1','name2','name3', 'name4'];
     var list='';
     for(var i=0; i<names.length; i++){
     list += '<li>' + names[i] + '<li>';
-}
-var ul= document.getElementById('namelist');
-ul.innerHTML=list;
+    }
+    var ul= document.getElementById('namelist');
+    ul.innerHTML=list;
+                }
+            }
+    };
+    
+    request.open('GET','http://lalitjad98.imad.hasura-app.io/counter',true);
+    request.send(null);
+
 };
 
