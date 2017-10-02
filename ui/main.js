7981caa2ed998c79eb357154e6cef170ca7a656e
lalitjad98 <30873825+lalitjad98@users.noreply.github.com>
2017-10-02 19:11:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,8 +20,6 @@ button.onclick = function() {
     
 };
 
-var nameInput= document.getElementById('name');
-var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
     
@@ -43,6 +41,9 @@ submit.onclick=function(){
             }
     };
     
+    var nameInput= document.getElementById('name');
+    var name=nameInput.value;
+
     request.open('GET','http://lalitjad98.imad.hasura-app.io/submit-name?name='+ name ,true);
     request.send(null);
 
